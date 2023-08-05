698b0da1156868323eb525ea351629e768062ebb
Andrei Horak <linkyndy@gmail.com>
2023-08-05 15:35:28 +0200

    Add configuration and logger

--- lib/notions.rb
+++ lib/notions.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
+require 'notions/configuration'
+require 'notions/logger'
 require 'notions/version'
 
 module Notions
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield configuration
+  end
+
+  def self.logger
+    @logger ||= Notions::Logger.new(STDOUT,
+      level: Notions::Logger::INFO
+    )
+  end
 end
---
+++ lib/notions/configuration.rb
@@ -0,0 +1,4 @@
+module Notions
+  class Configuration
+  end
+end
